9e27889bc1ef76184b4f446507d1aa6ad0366a61
ruchi <02ruchi2002@gmail.com>
2025-01-28 22:10:00 +0530

    integrated code editor

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react"
+import Client from "../components/Client"
+import Editor from "../components/Editor"
 
 const EditorPage = () =>{
+    const [clients,setClients] = useState([
+        {socketId:1,userName:"rohit"},
+        {socketId:2,userName:"anjali"},
+    ])
     return(
-        <>
-        <h1>editor page</h1>
-        </>
+        <div className="mainWrap">
+           <div className="aside">
+            <div className="asideInner">
+                <div className="logo">
+                    <img className="logoImage" src="" alt="logo" />
+                </div>
+                <h3>connected</h3>
+                <div className="clientsList">
+                    {
+                        clients?.map((client)=><Client
+                        key={client.socketId}
+                         username={client.userName}
+                         />)
+                    }
+                </div>
+            </div>
+
+            <button className="btn copyBtn">Copy ROOM ID</button>
+
+            <button className="btn leaveBtn">Leave</button>
+           </div>
+
+           <div className="editorWrap">
+            <Editor/>
+           </div>
+        </div>
     )
 }
 
